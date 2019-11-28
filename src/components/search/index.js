ae83486880f42e60397953f18414f579c6c8cf48
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-28 16:58:35 +0000

    Added basic layout and search bar

---
+++ src/components/search/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search.scss';
+
+export default class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <input className="search-bar"></input>
+            </div>
+        )
+    }
+}
