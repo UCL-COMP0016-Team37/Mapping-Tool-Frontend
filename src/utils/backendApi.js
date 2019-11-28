a557408f7d2cd1adc840f696e613962d393b2348
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-28 17:57:53 +0000

    Added getmap api

--- src/utils/backendApi.js
+++ src/utils/backendApi.js
@@ -1,4 +1,11 @@
 import Axios from 'axios';
 
-const url = '';
+const base_url = '';
 
+const API = {
+    getMap: () => {
+        return Axios.get(base_url + '/map');
+    },
+};
+
+export default API;
