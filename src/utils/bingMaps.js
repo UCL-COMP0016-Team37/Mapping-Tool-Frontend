a5491825b330ee3b2ad331ac4e3337116330ff20
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-28 17:17:57 +0000

    Added different key

--- src/utils/bingMaps.js
+++ src/utils/bingMaps.js
@@ -1,3 +1,3 @@
-const API_KEY = 'AkOz-wsnQOsdmBvNF4Qhk6ivv5Mcx1jeA16Sl53rxJv2kDFhjUpRZESfai4BXrf2';
+const API_KEY = 'AsSxctlp457kgvDJhiZaggsRjYJgWaGhJuW2QFkpgTsx0d1Gd4ux9Bez0y4zUufX';
 
 export default API_KEY;
