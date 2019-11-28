5b77877b21871a8a83bf6207424ea8a597dd2c0c
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-11-28 17:35:52 +0000

    Added test map

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -4,10 +4,23 @@ import { ReactBingmaps } from 'react-bingmaps';
 import API_KEY from '../../utils/bingMaps';
 
 export default class Map extends React.Component {
+    callBackMethod() {
+        console.log('test');
+    }
+
     render() {
         return <ReactBingmaps 
                 bingmapKey={API_KEY}
                 center={[13.0827, 80.2707]}
+                pushPins = {
+                    [
+                      {
+                        "location":[13.0827, 80.2707],
+                        "option":{ color: 'red' },
+                        "addHandler": {"type" : "click", callback: this.callBackMethod }
+                      }
+                    ]
+                  }
             />;
     }
 }
