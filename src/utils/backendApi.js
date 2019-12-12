655fa11c42fab98eae6371d4503cf022fc5caf81
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-12-12 17:25:09 +0000

    Added fake search api

--- src/utils/backendApi.js
+++ src/utils/backendApi.js
@@ -1,10 +1,25 @@
 import Axios from 'axios';
 
-const base_url = '';
+const base_url = 'https://mapping-tool-api.azurewebsites.net';
 
 const API = {
     getMap: () => {
-        return Axios.get(base_url + '/map');
+        return Axios.get(base_url + '/greeting');
+    },
+    getSearch: (term) => {
+        console.log(term);
+        return Promise.resolve({
+            data: [
+                {
+                    title: 'Project Test',
+                    description: 'ASJDOASDasdlaslkdajsdlkasdlkasdjalskd',
+                },
+                {
+                    title: 'Project Test 2',
+                    description: 'tjasdajldalkjsdaklsdlkjasdl',
+                },
+            ],
+        });
     },
 };
 
