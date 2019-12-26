6241a54d949081b9b2e6b7cbbcc6d295827999c5
Ruairidh Williamson <williamsonruairidh@gmail.com>
2019-12-26 16:02:46 +0000

    Redesigned advanced search

--- src/components/filterSearch/index.js
+++ src/components/filterSearch/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
+
 import './filterSearch.scss';
 import history from 'utils/history';
 
@@ -7,9 +9,9 @@ export default class filterSearch extends React.Component {
         super(props);
         this.state = {
             country: '',
-            sectorgroup: '',
-            startyear: '',
-            endyear: '',
+            sector: '',
+            startYear: '1990',
+            endYear: '1990',
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -19,38 +21,44 @@ export default class filterSearch extends React.Component {
     }
 
     handleSubmit(){
-        history.push('/search-results/?country='+ this.state.country +
-            '&sectorgroup=' + this.state.sectorgroup + 
-            '&startyear=' + this.state.startyear +
-            '&endyear=' + this.state.endyear);
+        history.push(
+            '/search-results/?country='+ this.state.country +
+            '&sector=' + this.state.sector + 
+            '&start=' + this.state.startYear +
+            '&end=' + this.state.endYear,
+        );
     }
 
     render() {
         const years = [1990,1991,1992,1993,2000,2018,2019,2020];
-        let thisyear = years.map(year=> <option key={year} value={year}>{year}</option>);
+        const yearsOptions = years.map(year=> <option key={year} value={year}>{year}</option>);
         return(
             <div className="form">
-                <form onSubmit={e=> this.handleSubmit(e)}>
-                    <label>Recipient country: </label>
-                    <input className="multiple" type="text" name="country" onChange={this.handleChange} />
-                   
-                    <label>Sector Group:</label>
-                    <input className="multiple" type="text" name="sectorgroup" onChange={this.handleChange} />
-                    
-                    <label className="years">Year Range </label>
-                    <label>Start Year:</label>
-                    <select className="multiple" name="startyear">
-                        {thisyear}
-                    </select>
-                        
-                    <label>End Year:</label>
-                    <select className="multiple" name="endyear">
-                        {thisyear}
-                    </select>
-                        
-                   
-                    <input type="submit" value="Submit" />
-                </form>
+                <Form onSubmit={e=> this.handleSubmit(e)}>
+                    <Form.Group controlId="country">
+                        <Form.Label>Recipient country: </Form.Label>
+                        <Form.Control type="text" name="country" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="sector">
+                        <Form.Label>Sector:</Form.Label>
+                        <Form.Control type="text" name="sector" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="startYear">
+                            <Form.Label>Start Year</Form.Label>
+                            <Form.Control as="select" name="startYear" onChange={this.handleChange}>
+                                {yearsOptions}
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="endYear">
+                            <Form.Label>End Year</Form.Label>
+                            <Form.Control as="select" name="endYear" onChange={this.handleChange}>
+                                {yearsOptions}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form.Row>
+                    <Button variant="primary" type="submit">Search</Button>
+                </Form>
             </div>
         ) ;
     }
