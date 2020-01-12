729c84e1174d129373d54d9337be58de77729e9c
Ruairidh Williamson <williamsonruairidh@gmail.com>
2020-01-12 13:15:33 +0000

    Fixed dependencies and eslint problems

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Chart from 'chart.js'
+import Chart from 'chart.js';
 
 import './chart.scss';
 import API from 'utils/backendApi';
@@ -8,17 +8,16 @@ export default class chart extends React.Component{
     constructor(props) {
         super(props);
         this.state = {results: []};
+        this.chartRef = React.createRef();
         API.getSearch('test').then((response) => {
             this.setState({ results: response.data });
         });
     }
 
-    chartRef = React.createRef();
-
-    
     componentDidUpdate(){
-        const country = this.state.results.map(country => country)// ['Afghanistan','Albania','Algeria','Andorra','Angola','UK']
-        var ctx = 'myChart';
+        const country = this.state.results.map(country => country);
+        // ['Afghanistan','Albania','Algeria','Andorra','Angola','UK']
+        let ctx = 'myChart';
         new Chart(ctx, {
             // The type of chart we want to create
             type: 'doughnut',
@@ -28,22 +27,23 @@ export default class chart extends React.Component{
                 labels: country,
                 datasets: [{
                     borderColor : 'grey',
-                    data: [10, 5, 2, 20, 30, 45]
-                }]
+                    data: [10, 5, 2, 20, 30, 45],
+                }],
             },
 
             // Configuration options go here
-            options: {}
-        })}
-        render() {
-            return (
-                <div >
-                    <canvas
-                        id="myChart"
-                        ref={this.chartRef}
-                    />
-                </div>
-            )
-        }
-   
+            options: {},
+        });
+    }
+
+    render() {
+        return (
+            <div >
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        );
+    }
 }
