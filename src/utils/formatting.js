8e8e62d5819fae419fb2aa5d84cf36935bbf884c
Ruairidh Williamson <williamsonruairidh@gmail.com>
2020-02-03 12:11:49 +0000

    Improved project page formatting

---
+++ src/utils/formatting.js
@@ -0,0 +1,31 @@
+export function formatMoney(amount, currency) {
+    if (amount === undefined) {
+        return '';
+    }
+    return `${mapCurrencySymbol(currency)} ${splitThousands(amount, ',')} ${currency}`;
+}
+
+function mapCurrencySymbol(currencyCode) {
+    switch (currencyCode.toUpperCase()) {
+    case 'USD': return '$';
+    case 'GBP': return '£';
+    default: return '';
+    }
+}
+
+function splitThousands(value, character = ' ', splitCount = 3) {
+    if (typeof value == 'string') {
+        value = parseFloat(value);
+    }
+    value = value.toString();
+    let decimalIndex = value.indexOf('.');
+    if (decimalIndex == -1) {
+        decimalIndex = value.length;
+    }
+    decimalIndex -= splitCount;
+    while (decimalIndex >= 0) {
+        value = value.slice(0, decimalIndex) + character + value.slice(decimalIndex);
+        decimalIndex -= splitCount;
+    }
+    return value;
+}
