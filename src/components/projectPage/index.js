1d9b22d1ca3f707075b2a93a541a90e055b7da84
Ruairidh Williamson <williamsonruairidh@gmail.com>
2020-02-14 16:19:42 +0000

    Added basic info to project page

--- src/components/projectPage/index.js
+++ src/components/projectPage/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Button, Row, Col } from 'react-bootstrap';
+import { Container, Button, Row, Col, ButtonGroup, Dropdown } from 'react-bootstrap';
 
 import API from 'utils/backendApi';
-// import { formatMoney } from 'utils/formatting';
+import { formatMoney } from 'utils/formatting';
 
-// function getNarrative(narrative) {
-//     return narrative.narratives[0].text;
-// }
+
+function getNar(narrative, lang = 'en') {
+    if (!narrative.narratives) {
+        return '';
+    }
+    return narrative.narratives.find(item => item.language.code == lang).text;
+}
+
+function getNarArray(narrativeArray, lang = 'en') {
+    return narrativeArray.map(item => getNar(item, lang));
+}
+
+function getMoney(value) {
+    return formatMoney(value.value, value.currency.code);
+}
 
 export default class projectPage extends React.Component{
     constructor(props) {
@@ -27,12 +39,33 @@ export default class projectPage extends React.Component{
             return <></>;
         }
         return <Container className="text-left">
-            <h1>Title</h1>
-            <h3>Reported By Reporter</h3>
-            <h3>Organisations</h3>
-            <h3>Locations</h3>
-            <h4>Total Budget</h4>
-            <h4>IATI Identifier</h4>
+            <h6>Title</h6>
+            <h4>{getNar(results.title)}</h4>
+            <Dropdown.Divider/>
+            <h6>Locations</h6>
+            <h4>{getNarArray(results.locations.map(item => item.name)).join(', ')}</h4>
+            <Dropdown.Divider/>
+            <h6>Reported By</h6>
+            <h4>{getNar(results.reporting_org)}</h4>
+            <Dropdown.Divider/>
+            <h6>Organisations</h6>
+            <h4>{getNarArray(results.participating_organisations).join(', ')}</h4>
+            <Dropdown.Divider/>
+            <h6>Budget</h6>
+            <h4>{results.budgets.map(item => getMoney(item.value)).join(', ')}</h4>
+            <Dropdown.Divider/>
+            <h6>Identifier</h6>
+            <h4>{results.iati_identifier}</h4>
+
+            <div className="d-flex flex-column">
+                <ButtonGroup className="mt-3">
+                    <Button variant="link">Overview</Button>
+                    <Button variant="link">Budgets</Button>
+                    <Button variant="link">Transactions</Button>
+                </ButtonGroup>
+            </div>
+            <h2>Overview</h2>
+            {getNarArray(results.descriptions).map((i, j) => <p key={j}>{i}</p>)}
             <p>Sectors: </p>
             <Row className="text-center"><Col>Start Date</Col><Col>End Date</Col></Row>
             <p><Button variant="primary">Project Website</Button></p>
