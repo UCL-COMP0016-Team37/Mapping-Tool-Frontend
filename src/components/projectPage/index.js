74cc3c16472bb45398ca4ffb7b7f28e07e1032e7
Ruairidh Williamson <williamsonruairidh@gmail.com>
2020-02-14 18:35:39 +0000

    Added sections to project page

--- src/components/projectPage/index.js
+++ src/components/projectPage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Button, Row, Col, ButtonGroup, Dropdown } from 'react-bootstrap';
+import { Container, Button, Row, Col, ButtonGroup, Table, Dropdown } from 'react-bootstrap';
 
 import API from 'utils/backendApi';
 import { formatMoney } from 'utils/formatting';
 import { Link } from 'react-router-dom';
 
+const Divider = Dropdown.Divider;
 
 function getNar(narrative, lang = 'en') {
     if (!narrative.narratives) {
@@ -57,19 +58,19 @@ export default class projectPage extends React.Component{
                 <Col>
                     <h6>Title</h6>
                     <h5>{getNar(results.title)}</h5>
-                    <Dropdown.Divider/>
+                    <Divider/>
                     <h6>Locations</h6>
                     <h5>{getNarArray(results.locations.map(item => item.name)).join(', ')}</h5>
-                    <Dropdown.Divider/>
+                    <Divider/>
                     <h6>Reported By</h6>
                     <h5>{getNar(results.reporting_org)}</h5>
-                    <Dropdown.Divider/>
+                    <Divider/>
                     <h6>Organisations</h6>
                     <h5>{joinBR(unique(getNarArray(results.participating_organisations)))}</h5>
-                    <Dropdown.Divider/>
+                    <Divider/>
                     <h6>Budget</h6>
                     <h5>{joinBR(results.budgets.map(item => getMoney(item.value)))}</h5>
-                    <Dropdown.Divider/>
+                    <Divider/>
                     <h6>Identifier</h6>
                     <Button variant="link" to={`/project-page/${results.iati_identifier}`} as={Link}><h5>{results.iati_identifier}</h5></Button>
                 </Col>
@@ -78,22 +79,55 @@ export default class projectPage extends React.Component{
             <div className="d-flex flex-column">
                 <ButtonGroup className="mt-3">
                     <Button variant="link" href="#overview">Overview</Button>
-                    <Button variant="link">Budgets</Button>
-                    <Button variant="link">Transactions</Button>
+                    <Button variant="link" href="#locations">Locations</Button>
+                    <Button variant="link" href="#budgets">Budgets</Button>
+                    <Button variant="link" href="#transactions">Transactions</Button>
                 </ButtonGroup>
             </div>
             <div id="overview">
-                <h2>Overview</h2>
+                <h4>Overview</h4>
             </div>
             {getNarArray(results.descriptions).map((i, j) => <p key={j}>{i}</p>)}
-            <p>Sectors: </p>
             <Row className="text-center"><Col>Start Date</Col><Col>End Date</Col></Row>
-            <p><Button variant="primary">Project Website</Button></p>
-            <p>Project Description</p>
-            <p>Contact:</p>
-            <p>Name:</p>
-            <p>Position:</p>
-            <p>Email:</p>
+
+            <div id="locations">
+                <h4>Locations</h4>
+                {results.locations.map(item =>
+                    <>
+                        <h6>{getNar(item.name)}</h6>
+                        {getNar(item.description)}
+                        <Divider/>
+                    </>,
+                )}
+            </div>
+
+            <div id="budgets">
+                <h4>Budgets</h4>
+                <Table hover>
+                    <thead>
+                        <tr>
+                            <th>Type</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {results.budgets.map((item, i) =>
+                            <tr key={i}>
+                                <td>{item.type.name}</td>
+                                <td>{item.period_start}</td>
+                                <td>{item.period_end}</td>
+                                <td>{getMoney(item.value)}</td>
+                            </tr>,
+                        )}
+                    </tbody>
+                </Table>
+            </div>
+
+            <div id="transactions">
+                <h4>Transactions</h4>
+            </div>
         </Container>;
     }
 
