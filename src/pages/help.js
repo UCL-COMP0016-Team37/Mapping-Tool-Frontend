3d3aeded7b8a7d36292cd01afbed2be0342fc770
afiq <mbinsams@gitlab.cs.ucl.ac.uk>
2020-03-25 23:38:42 +0000

    redo help page - need styling

--- src/pages/help.js
+++ src/pages/help.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import pinimg from '../assets/get-started/pin-hover.png';
-import search from '../assets/get-started/search.png';
-import analysis from '../assets/get-started/analysis-page.png';
-import project from '../assets/get-started/project-page.png';
-import searchresult from '../assets/get-started/search-result.png';
-import top100 from '../assets/get-started/top-100.png';
 
 export default class LocationPage extends React.Component {
     render() {
@@ -21,7 +15,6 @@ export default class LocationPage extends React.Component {
                 user will go to search results page which shows all project in the
                 country
             </p>
-            <img src={pinimg} alt="pin" width='400vw'/>
 
             <h3>PROJECT PAGE</h3>
             <p>
@@ -29,28 +22,27 @@ export default class LocationPage extends React.Component {
                 Among the data shown includes, location, reporting organisations,
                 budget, overview and transactions.
             </p>
-            <img src={project} alt="project"  margin-left='20vw' width='400vw'/>
+
             <h3>TOP 100</h3>
             <p>
                 Top 100 page shows the list Top 100 donors/reportiong organisations
                 including their number of projects reported.
             </p>
-            <img src={top100} alt="top-100"  margin-left='20vw' width='400vw'/>
+
             <h3>SEARCH</h3>
             <p>
                 In search, user can choose to search either by keyword, sectors or
                 country or any combination of all of them. User will then be redirected
                 to the search results page to view the project list.
             </p>
-            <img src={search} alt="search"  margin-left='20vw' width='400vw'/>
-            <img src={searchresult} alt="searchresult"  margin-left='20vw' width='400vw'/>
+
             <h3>ANALYSIS</h3>
             <p>
                 Analysis helps aggregate all the data of the search category and graph
                 it so that user would be able to get the general overview of projects in
                 a country or projects in a sector or a combination of both.
             </p>
-            <img src={analysis} alt="analysis"  margin-left='20vw' width='400vw'/>
+
         </div>;
     }
 }
